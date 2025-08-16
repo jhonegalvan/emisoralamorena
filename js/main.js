b6e5c96a4346e93ed9ce8e4fe0b97c99d4848dda
jhonegalvan <73579471+jhonegalvan@users.noreply.github.com>
2025-08-16 12:46:31 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
     // --- [CONFIGURAÇÕES] ----------------------------------------------- 
 
     const API_KEY_LYRICS = "";
-    const API_URL = "";
+    const API_URL = "https://twj.es/metadata/?url=https://stream.zeno.fm/eccwblcjp3wuv";
     const TIME_TO_REFRESH = window?.streams?.timeRefresh || 1000;
 
     // Verificar si el navegador soporta Service Worker
